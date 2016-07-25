890ac88d0033d437b209546928b72ce1201dba32
wentworth99 <wentworthjohn99@gmail.com>
2016-07-24 23:23:57 -0500

    Add files via upload

---
+++ KMHtoMPH.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<?php
+
+$kmh = 0;
+
+
+?>
+
+<form action="welcome.php" method="post">
+KMH: <input type="text" name="kmh" value="<?php echo $kmh;?>"><br>
+<?php echo "Press Enter to convert KMH to MPH"?>
+</form>
+
+
+
+</body>
+</html>
